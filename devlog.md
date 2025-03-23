9ab72a0658be33ad478824f20e3b6ae96bbea5a6
HackHuang <GoogTech@outlook.com>
2025-03-23 16:40:50 +0800

    desgin the agent for tools and summarizer

--- devlog.md
+++ devlog.md
@@ -16,3 +16,71 @@ executor ---> {'tools': ['Nmap', 'Nmap', 'Nessus/OpenVAS'], 'command': ['nmap 10
 
 User: 
 ```
+
+2025/03/23 :
+```sh
+hackhuang@HackHuang hackBot % python3 hackbot.py 
+<IPython.core.display.Image object>
+
+User: hello are you ok.
+
+planner ---> {'tasks': []}
+
+User: pls help me scan the ip: 101.121.121.111 and find the open port and os type.
+
+planner ---> {'tasks': [{'task_id': 1, 'task': 'Perform a port scan on IP 101.121.121.111 to identify open ports.'}, {'task_id': 2, 'task': 'Determine the operating system type of the device at IP 101.121.121.111.'}]}
+content="\n        You are a executor in penetration testing processes,\n        Your duty is to generate each attack command based on every task in the task list,\n        Of course, the prerequisite is that the task can generate the attack command,\n        then execute them by calling the Tool Agent until every task is done.\n        \n        The task list: [{'task_id': 1, 'task': 'Perform a port scan on IP 101.121.121.111 to identify open ports.'}, {'task_id': 2, 'task': 'Determine the operating system type of the device at IP 101.121.121.111.'}]\n        " additional_kwargs={} response_metadata={}
+
+executor ---> {'tools': ['nmap', 'nmap'], 'command': [['nmap -sS 101.121.121.111'], ['nmap -O 101.121.121.111']]}
+
+User: 
+```
+
+2025/03/23 :
+```sh
+hackhuang@HackHuang hackBot % python3 hackbot.py 
+<IPython.core.display.Image object>
+
+User: scan the ip: 10.2.3.4, aim to find the open ports and os type.
+
+planner ---> {'tasks': [{'task_id': 1, 'task': 'Perform a port scan on IP 10.2.3.4 to identify open ports.'}, {'task_id': 2, 'task': 'Determine the operating system type of the target IP 10.2.3.4.'}]}
+
+executor ---> {'commands': ['nmap 10.2.3.4', 'nmap -O 10.2.3.4'], 'tools_name': ['nmap', 'nmap']}
+
+tools ---> content='' additional_kwargs={'tool_calls': [{'id': 'call_1YjaCKVIA3yraSdYvimUFFmY', 'function': {'arguments': '{"commands": "nmap 10.2.3.4"}', 'name': 'terminal'}, 'type': 'function'}, {'id': 'call_vhwhEx76n58qFQd9nAzPMhmv', 'function': {'arguments': '{"commands": "nmap -O 10.2.3.4"}', 'name': 'terminal'}, 'type': 'function'}], 'refusal': None} response_metadata={'token_usage': {'completion_tokens': 65, 'prompt_tokens': 193, 'total_tokens': 258, 'completion_tokens_details': {'accepted_prediction_tokens': None, 'audio_tokens': 0, 'reasoning_tokens': 0, 'rejected_prediction_tokens': None}, 'prompt_tokens_details': {'audio_tokens': 0, 'cached_tokens': 0}}, 'model_name': 'gpt-4o-mini-2024-07-18', 'system_fingerprint': 'fp_ded0d14823', 'id': 'chatcmpl-BE99prB5RpfZMxCN3yV85SBPSowHm', 'finish_reason': 'tool_calls', 'logprobs': None} id='run-6f3ec599-9222-4e6f-9324-29b8a89c7376-0' tool_calls=[{'name': 'terminal', 'args': {'commands': 'nmap 10.2.3.4'}, 'id': 'call_1YjaCKVIA3yraSdYvimUFFmY', 'type': 'tool_call'}, {'name': 'terminal', 'args': {'commands': 'nmap -O 10.2.3.4'}, 'id': 'call_vhwhEx76n58qFQd9nAzPMhmv', 'type': 'tool_call'}] usage_metadata={'input_tokens': 193, 'output_tokens': 65, 'total_tokens': 258, 'input_token_details': {'audio': 0, 'cache_read': 0}, 'output_token_details': {'audio': 0, 'reasoning': 0}}
+tools_dict ---> {'tavily_search_results_json': TavilySearchResults(max_results=2, api_wrapper=TavilySearchAPIWrapper(tavily_api_key=SecretStr('**********'))), 'terminal': ShellTool(process=<langchain_experimental.llm_bash.bash.BashProcess object at 0x1059aae40>)}
+Executing command:
+ ['nmap 10.2.3.4']
+selected_tool ---> terminal
+selected_tool_response ---> content='/bin/sh: nmap: command not found\n' name='terminal' tool_call_id='call_1YjaCKVIA3yraSdYvimUFFmY'
+Executing command:
+ ['nmap -O 10.2.3.4']
+selected_tool ---> terminal
+selected_tool_response ---> content='/bin/sh: nmap: command not found\n' name='terminal' tool_call_id='call_vhwhEx76n58qFQd9nAzPMhmv'
+
+......
+```
+
+2025/03/23 :
+```sh
+hackhuang@HackHuang hackBot % python3 hackbot.py
+<IPython.core.display.Image object>
+
+User: scan the ip: 100.200.300.400, aim to find the open ports and os type.
+
+planner ---> {'messages': 'Here is the task list for scanning the IP address to find open ports and OS type.', 'tasks': ['1. Use Nmap to perform a basic scan to identify open ports on the IP address 100.200.300.400.', '2. Use Nmap with OS detection flag to determine the operating system type of the IP address 100.200.300.400.']}
+
+executor ---> {'tools_name': ['Nmap', 'Nmap'], 'commands': ['nmap 100.200.300.400', 'nmap -O 100.200.300.400']}
+
+tools_dict ---> {'tavily_search_results_json': TavilySearchResults(max_results=2, api_wrapper=TavilySearchAPIWrapper(tavily_api_key=SecretStr('**********'))), 'terminal': ShellTool(process=<langchain_experimental.llm_bash.bash.BashProcess object at 0x10779f0e0>)}
+selected_tool ---> terminal
+Executing command:
+ ['nmap 100.200.300.400']
+selected_tool_response ---> content='/bin/sh: nmap: command not found\n' name='terminal' tool_call_id='call_vpnUnFiLY8sF9Qn8hVjERit6'
+selected_tool ---> terminal
+Executing command:
+ ['nmap -O 100.200.300.400']
+selected_tool_response ---> content='/bin/sh: nmap: command not found\n' name='terminal' tool_call_id='call_mEbfX26LMPKAc7akOS2uvq8G'
+
+summarizer ---> {'fail_solution': "The command execution failed because the 'nmap' tool is not installed on the system. To resolve this issue, you need to install 'nmap' by running the following command in the terminal:\n\n```bash\nsudo apt-get install nmap\n```\n\nAfter installation, try running the commands again."}
+```
--- hackbot.py
+++ hackbot.py
@@ -3,28 +3,40 @@ from typing_extensions import TypedDict, Annotated
 from langgraph.graph.message import add_messages
 from langgraph.graph import StateGraph, START, END
 from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_community.tools import ShellTool
 from langgraph.prebuilt import ToolNode, tools_condition
 from langchain_openai import ChatOpenAI
 from langgraph.types import Command
 from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
-from typing import Final, Union, Literal, List
+from typing import Final, Union, Literal, List, Dict
 from dotenv import load_dotenv
 from IPython.display import Image, display
 import os
+import warnings
+
+warnings.filterwarnings(
+    action="ignore", 
+    message="The shell tool has no safeguards by default. Use at your own risk.", 
+    category=UserWarning
+)
 
 load_dotenv()
-
 os.environ["TAVILY_API_KEY"] = os.getenv("TAVILY_API_KEY")
-def web_reasearch() -> TavilySearchResults:
-    tool = TavilySearchResults(max_results=2)
-    return tool
 
-tools: List = [web_reasearch()]
+def web_reasearch() -> TavilySearchResults:
+    web_search_tool = TavilySearchResults(max_results=2)
+    return web_search_tool
+
+def shell_executor() -> ShellTool:
+    shell_tool = ShellTool()
+    return shell_tool
+
+tools: List = [web_reasearch(), shell_executor()]
 
 class ModelTools():
     DEFAULT_MODEL: Final = "openai"
     MODEL_TEMPERATURE: Final = 0
-    CHATGPT_MODEL_NAME: Final = "gpt-4o"
+    CHATGPT_MODEL_NAME: Final = "gpt-4o" # or gpt-4o-mini
 
     @staticmethod
     def _get_llm_with_tools(model_name: str = DEFAULT_MODEL) -> Union[ChatOpenAI]:
@@ -44,6 +56,9 @@ class AgentState(TypedDict):
     messages: Annotated[list, add_messages, "the agent messages"]
     user_input: Annotated[str, "the user input information"]
     tasks: Annotated[list, "the generated task list according to the Human Message"]
+    tools_name: Annotated[list, "the name of each hack tool will be used according to the corresponding task"]
+    commands: Annotated[list, "each command will be generated based on the corresponding task"]
+    command_results: Annotated[Dict[str, str], "each result of every executed command"]
     pass
 
 class PlannerOuputSchema(TypedDict):
@@ -52,15 +67,20 @@ class PlannerOuputSchema(TypedDict):
     pass
 
 class ExecutorOutputSchema(TypedDict):
-    tools: Annotated[list, "the name of each hack tool will be used according to the corresponding task"]
-    command: Annotated[list, "each command will be generated based on the corresponding task"]
+    # message: Annotated[str, "the thought process"]
+    tools_name: Annotated[list, "the name of each hack tool will be used according to the corresponding task"]
+    commands: Annotated[list, "each command will be generated based on the corresponding task"]
+    # command_result: Annotated[list, "the result of each executed command"]
     pass
+
+# class ToolsOuputSchema(TypedDict):
+#     command_results: Annotated[Dict[str, str], "each result of every executed command"]
 
 class SummarizerOutputSchema(TypedDict):
-    pass
+    fail_solution: Annotated[str, "the solution for the failures"]
 
 class Agent():
-    def planner(state: AgentState) -> Command[Literal["executor", END]]:
+    def planner(state: AgentState) -> Command[Literal["executor", "reporter"]]:
         PLANNER_SYSTEM_PROMPT: Final = SystemMessage(content="""
         You are a planner in penetration testing processes,
         Your duty is generate precise and concise task list according to the Human Message
@@ -70,6 +90,8 @@ class Agent():
         Notes: 
         1.Please include the series number in the task header.
         2.Please try to finish as soon as possible by using the quickest way.
+        3.Do not generate tasks that cannot be translated into attack commands, 
+          For example, documenting the findings and preparing a report.
         """
         )
         user_input_message = HumanMessage(content=str(state["user_input"]))
@@ -90,35 +112,103 @@ class Agent():
     def executor(state: AgentState) -> Command[Literal["tools", "summarizer"]]:
         EXECUTOR_SYSTEM_PROMPT: Final = SystemMessage(content=f"""
         You are a executor in penetration testing processes,
-        Your duty is to generate each attack command based on every task in the task list
-        then execute them by calling the Tool Agent until every task is done.
-        
-        The task list: {state['tasks']}
+        Your duty is to generate each attack command based on every task in the task list,
+        Of course, the prerequisite is that the task can generate the attack command.
+
+        The task list: {state['tasks']}        
         """
         )
-        print(EXECUTOR_SYSTEM_PROMPT)
-        messages = [EXECUTOR_SYSTEM_PROMPT]
-        llm_with_tools = ModelTools._get_llm_with_tools().with_structured_output(schema=ExecutorOutputSchema)
-        response = llm_with_tools.invoke(messages)
-        print(f"\nexecutor ---> {response}")
+        if "command_results" not in state:
+            messages = [EXECUTOR_SYSTEM_PROMPT]
+            llm_with_tools = ModelTools._get_llm_with_tools().with_structured_output(schema=ExecutorOutputSchema)
+            response = llm_with_tools.invoke(messages)
+            print(f"\nexecutor ---> {response}")
+            return Command(
+                goto="tools",
+                update={
+                    "tools_name": response["tools_name"],
+                    "commands": response["commands"],
+                    "messages": [AIMessage(content=f"go to tools")]
+                },
+            )
         return Command(
-            goto="tools",
+            goto="summarizer",
+            update={
+                # "command_results": state["command_results"],
+                "messages": [AIMessage(content=f"go to summarizer")]
+            }
+        )
+
+    def tools(state: AgentState) -> Command[Literal["executor"]]:
+        TOOLS_SYSTEM_PROMPT: Final = SystemMessage(content=f"""
+        You are a command executor in penetration testing processes,
+        Your duty is to use the one of hack tools in {state["tools_name"]}
+        to execute every attack command in {state["commands"]},
+        and you have access to bash shell using the shell_tool.     
+        """
+        )
+        messages = [TOOLS_SYSTEM_PROMPT]
+        llm_with_tools = ModelTools._get_llm_with_tools()
+        response = llm_with_tools.invoke(messages)
+        
+        tools_dict = {tool.name: tool for tool in tools}
+        print(f"\ntools_dict ---> {tools_dict}")
+
+        messages.append(response)
+        for tool_call in response.tool_calls:
+            selected_tool = tool_call["name"].lower()
+            print(f"selected_tool ---> {selected_tool}")
+            selected_tool_obj = tools_dict[selected_tool]
+            selected_tool_response = selected_tool_obj.invoke(tool_call)
+            messages.append(selected_tool_response)
+            print(f"selected_tool_response ---> {selected_tool_response}")
+
+        return Command(
+            goto="executor",
+            update={
+                    # "command_results": response["command_results"],
+                    "command_results": messages,
+                    "messages": [AIMessage(content=f"go to executor")]
+            },
         )
 
     def summarizer(state: AgentState) -> Command[Literal["planner"]]:
+        SUMMARIZER_SYSTEM_PROMPT: Final = SystemMessage(content=f"""
+        You are a summarizer for command execution results in penetration testing processes,
+        Your duty is summarize the command execution results concisely, 
+        and if the command execution process fails, please provide the solution.
+
+        The command execution results: {state["command_results"]}
+        """
+        )
+        messages = [SUMMARIZER_SYSTEM_PROMPT]
+        llm_with_tools = ModelTools._get_llm_with_tools().with_structured_output(schema=SummarizerOutputSchema)
+        response = llm_with_tools.invoke(messages)
+        print(f"\nsummarizer ---> {response}")
         return Command(
             goto="planner",
+        )
+
+    def reporter(state: AgentState) -> Command[Literal[END]]:
+        return Command(
+            goto=END
         )
 
 graph_builder = StateGraph(state_schema=AgentState)
 graph_builder.add_node(node="planner", action=Agent.planner)
 graph_builder.add_node(node="executor", action=Agent.executor)
 graph_builder.add_node(node="summarizer", action=Agent.summarizer)
-tools_node = ToolNode(tools=tools)
-graph_builder.add_node(node="tools", action=tools_node)
+graph_builder.add_node(node="tools", action=Agent.tools)
+graph_builder.add_node(node="reporter", action=Agent.reporter)
+# tools_node = ToolNode(tools=tools)
+# graph_builder.add_node(node="tools", action=tools_node)
 graph_builder.add_edge(start_key=START, end_key="planner")
 # graph_builder.add_edge(start_key="tools", end_key="executor")
-# graph_builder.add_conditional_edges(source="executor", path=tools_condition)
+# graph_builder.add_conditional_edges(
+#     source="executor", 
+#     path=tools_condition,
+#     path_map={"tools": "tools", END: "summarizer"}
+# )
 graph = graph_builder.compile()
 
 class Test():
