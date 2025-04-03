a2e4f296b9c881fa7cae56cd72d80f60ccde174f
HackHuang <GoogTech@outlook.com>
2025-04-03 21:53:11 +0800

    update adaptive_rag.py

--- adaptive_rag.py
+++ adaptive_rag.py
@@ -5,14 +5,24 @@
 #                                                               #
 # Paper: https://arxiv.org/abs/2403.14403                       #
 #################################################################
-
-#######
-# LLM #
-#######
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from typing import Annotated, Sequence, Literal, Union, Final
 from dotenv import load_dotenv
 import os
+from typing import Literal
+from langchain_core.prompts import ChatPromptTemplate
+from pydantic import BaseModel, Field
+from langchain import hub
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.tools.tavily_search import TavilySearchResults
+from typing import List
+from typing_extensions import TypedDict
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import Chroma
+from langgraph.graph import END, StateGraph, START
+from IPython.display import Image, display
+from langchain.schema import Document
 
 load_dotenv()
 
@@ -55,12 +65,7 @@ class ModelTools():
 ################
 # Create Index #
 ################
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import WebBaseLoader
-from langchain_community.vectorstores import Chroma
-
 # Set embeddings
-# embd = OpenAIEmbeddings()
 embd = ModelTools.get_embed()
 
 # Docs to index
@@ -88,214 +93,14 @@ vectorstore = Chroma.from_documents(
 )
 retriever = vectorstore.as_retriever()
 
-#######################
-# LLMs 1: Tool Router #
-#######################
-from typing import Literal
-from langchain_core.prompts import ChatPromptTemplate
-from pydantic import BaseModel, Field
-
-# Data model
-class RouteQuery(BaseModel):
-    """Route a user query to the most relevant datasource."""
-    datasoure: Literal["vectorstore", "web_search"] = Field(
-        ...,
-        description="Given a user question choose to route it to web search or a vectorstore."
-    )
-
-# LLM with function call
-# llm = ChatOpenAI(
-#     model="gpt-4o-mini",
-#     temperature=0,
-# )
-llm = ModelTools.get_llm()
-structured_llm_router = llm.with_structured_output(RouteQuery)
-
-# Prompt
-system = """
-You are an expert at routing a user question to a vectorstore or web search.
-The vectorstore contains documents related to agents, prompt engineering, and adversarial attacks.
-Use the vectorstore for questions on these topics. Otherwise, use web-search.
-"""
-route_prompt = ChatPromptTemplate.from_messages([
-    ("system", system),
-    ("human", "{question}"),
-])
-
-question_router = route_prompt | structured_llm_router
-
-print(question_router.invoke({"question": "Who will the Bears draft first in the NFL draft?"}))
-print(question_router.invoke({"question": "What are the types of agent memory?"}))
-
-############################
-# LLMs 2: Retrieval Grader #
-############################
-# Date model
-class GradeDocuments(BaseModel):
-    """Binary score for relevance check on retrieved documents"""
-    binary_score: str = Field(
-        description="Documents are relevant to the question, 'yes' or 'no'"
-    )
-
-# LLM with function call
-# llm = ChatOpenAI(
-#     model="gpt-4o",
-#     temperature=0,
-# )
-llm = ModelTools.get_llm()
-structured_llm_grader = llm.with_structured_output(GradeDocuments)
-
-# Prompt
-system = """
-You are a grader assessing relevance of a retrieved document to a user question. \n
-If the document contains keyword(s) or semantic meaning related to the user question, grade it as relevant. \n
-Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question.
-"""
-grade_prompt = ChatPromptTemplate.from_messages([
-    ("system", system),
-    ("human", "Retrieved document: \n\n {document} \n\n User question: {question}")
-])
-
-retrieval_grader = grade_prompt | structured_llm_grader
-question = "agent memory"
-docs = retriever.invoke(question)
-doc_txt = docs[1].page_content
-print(retrieval_grader.invoke({"question": question, "document": doc_txt}))
-
-####################
-# LLMs 3: Generate #
-####################
-from langchain import hub
-from langchain_core.output_parsers import StrOutputParser
-
-# Prompt
-"""
-You are an assistant for question-answering tasks.
-Use the following pieces of retrieved context to answer the question.
-If you don't know the answer, just say that you don't konw.
-Use three sentences maximum and keep the answer concise.
-
-Question: {question}
-Context: {context}
-Answer:
-"""
-prompt = hub.pull("rlm/rag-prompt")
-# LLM
-# llm = ChatOpenAI(
-#     model="gpt-4o",
-#     temperature=0,
-# )
-llm = ModelTools.get_llm()
-# Post-processing
-def format_docs(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
-# Chain
-rag_chain = prompt | llm | StrOutputParser()
-# Run
-generation = rag_chain.invoke({"context": docs, "question": question})
-print(generation)
-
-################################
-# LLMs 4: Hallucination Grader #
-################################
-# Data model
-class GradeHallucinations(BaseModel):
-    """Binary score for hallucination present in generation answer."""
-    binary_score: str = Field(
-        description="Answer is grounded in the facts, 'yes' or 'no'"
-    )
-
-# LLM with function call
-# llm = ChatOpenAI(
-#     model="gpt-4o",
-#     temperature=0,
-# )
-llm = ModelTools.get_llm()
-structured_llm_grader = llm.with_structured_output(GradeHallucinations)
-
-# Prompt
-system = """
-You are a grader assessing whether an LLM generation is grounded in / 
-supported by a set of retrieved facts. \n
-
-Give a binary score 'yes' or 'no', 
-'Yes' means that the answer is grounded in / supported by the set of facts.
-"""
-hallucination_prompt = ChatPromptTemplate.from_messages([
-    ("system", system),
-    ("human", "Set of facts: \n\n {documents} \n\n LLM generation: {generation}")
-])
-
-hallucination_grader = hallucination_prompt | structured_llm_grader
-hallucination_grader.invoke({"documents": docs, "generation": generation})
-
-#########################
-# LLMs 5: Answer Grader #
-#########################
-# Data model
-class GradeAnswer(BaseModel):
-    """Binary score to assess answer addressses question"""
-    binary_score: str = Field(
-        description="Answer addresses the question, 'yes' or 'no'"
-    )
-
-# LLM with function call
-# llm = ChatOpenAI(
-#     model="gpt-4o",
-#     temperature=0,
-# )
-llm = ModelTools.get_llm()
-structured_llm_grader = llm.with_structured_output(GradeAnswer)
-
-# Prompt
-system = """
-You are a grader assessing whether an answer addresses / resolves a question \n
-Give a binary score 'yes' or 'no'.
-'Yes' means that the answer resolves the question.
-"""
-answer_prompt = ChatPromptTemplate.from_messages([
-    ("system", system),
-    ("human", "User question: \n\n {question} \n\n LLM generation: {generation}"),
-])
-
-answer_grader = answer_prompt | structured_llm_grader
-answer_grader.invoke({"question": question, "generation": generation})
-
-##############################
-# LLMs 6: Question Re-writer #
-##############################
-# LLM
-# llm = ChatOpenAI(
-#     model="gpt-4o",
-#     temperature=0,
-# )
-llm = ModelTools.get_llm()
-# Prompt
-system = """
-You a question re-writer that converts an input question to a better version that is optimized \n
-for vectorstore retrieval. Look at the input and try to reason about the underlying semantic intent / meaning.
-"""
-re_write_prompt = ChatPromptTemplate.from_messages([
-    ("system", system),
-    ("human", "Here is the initial question: \n\n {question} \n Formulate an improved question.")
-])
-
-question_rewriter = re_write_prompt | llm | StrOutputParser()
-question_rewriter.invoke({"question": question})
-
 #######################################
 # Web Search Tool: Question Re-writer #
 #######################################
-from langchain_community.tools.tavily_search import TavilySearchResults
-
 web_search_tool = TavilySearchResults(k=3)
 
 ######################
 # Define Graph State #
 ######################
-from typing import List
-from typing_extensions import TypedDict
-
 class GraphState(TypedDict):
     """Represents the state of our graph.
 
@@ -306,26 +111,44 @@ class GraphState(TypedDict):
     """
     question: str
     generation: str
-    document: List[str]
+    documents: List[str]
 
 #####################
 # Define Graph Flow #
 #####################
-from langchain.schema import Document
-
 def retrieve(state: GraphState):
     """Retrieve documents"""
     print(f"retrieve ————>")
     question = state["question"]
     # Retrieval
     documents = retriever.invoke(question)
-    return {"document": documents, "question": question}
+    return {"documents": documents, "question": question}
+
+class GradeDocuments(BaseModel):
+    """Binary score for relevance check on retrieved documents"""
+    binary_score: str = Field(
+        description="Documents are relevant to the question, 'yes' or 'no'"
+    )
 
 def grade_documents(state: GraphState):
+    # Prompt
+    system = """
+    You are a grader assessing relevance of a retrieved document to a user question. \n
+    If the document contains keyword(s) or semantic meaning related to the user question, grade it as relevant. \n
+    Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question.
+    """
+    grade_prompt = ChatPromptTemplate.from_messages([
+        ("system", system),
+        ("human", "Retrieved document: \n\n {document} \n\n User question: {question}")
+    ])
+    llm = ModelTools.get_llm()
+    structured_llm_grader = llm.with_structured_output(GradeDocuments)
+    retrieval_grader = grade_prompt | structured_llm_grader
+
     """Determines whether the retrieved documents are relevant to the question"""
     print(f"grade_documents ————>")
     question = state["question"]
-    documents = state["documents"]
+    documents = state["documents"] # TODO: bug here: KeyError('documents')
     # Score each doc
     filtered_docs = []
     for d in documents:
@@ -340,15 +163,43 @@ def grade_documents(state: GraphState):
     return {"documents": filtered_docs, "question": question}
 
 def generate(state: GraphState):
+    """
+    You are an assistant for question-answering tasks.
+    Use the following pieces of retrieved context to answer the question.
+    If you don't know the answer, just say that you don't konw.
+    Use three sentences maximum and keep the answer concise.
+
+    Question: {question}
+    Context: {context}
+    Answer:
+    """
+    prompt = hub.pull("rlm/rag-prompt")
+    def format_docs(docs):
+        return "\n\n".join(doc.page_content for doc in docs) # Post-processing
+    llm = ModelTools.get_llm()
+    rag_chain = prompt | llm | StrOutputParser()
+
     """Generate answer"""
     print(f"generate ————>")
     question = state["question"]
     documents = state["documents"]
     # RAG generation
     generation = rag_chain.invoke({"context": documents, "question": question})
+    # TODO: bug here: NameError("name 'generation' is not defined")
     return {"documents": documents, "question": question, "generation": generation}
 
 def transform_query(state: GraphState):
+    system = """
+    You a question re-writer that converts an input question to a better version that is optimized \n
+    for vectorstore retrieval. Look at the input and try to reason about the underlying semantic intent / meaning.
+    """
+    re_write_prompt = ChatPromptTemplate.from_messages([
+        ("system", system),
+        ("human", "Here is the initial question: \n\n {question} \n Formulate an improved question.")
+    ])
+    llm = ModelTools.get_llm()
+    question_rewriter = re_write_prompt | llm | StrOutputParser()
+
     """Transform the query to produce a better question"""
     print(f"transform_query ————>")
     question = state["question"]
@@ -370,7 +221,27 @@ def web_search(state):
 ################
 # Define Edges #
 ################
+class RouteQuery(BaseModel):
+    """Route a user query to the most relevant datasource."""
+    datasource: Literal["vectorstore", "web_search"] = Field(
+        ...,
+        description="Given a user question choose to route it to web search or a vectorstore."
+    )
+
 def route_question(state: GraphState):
+    system = """
+    You are an expert at routing a user question to a vectorstore or web search.
+    The vectorstore contains documents related to agents, prompt engineering, and adversarial attacks.
+    Use the vectorstore for questions on these topics. Otherwise, use web-search.
+    """
+    route_prompt = ChatPromptTemplate.from_messages([
+        ("system", system),
+        ("human", "{question}"),
+    ])
+    llm = ModelTools.get_llm()
+    structured_llm_router = llm.with_structured_output(RouteQuery)
+    question_router = route_prompt | structured_llm_router
+
     """Route question to web search or RAG"""
     print(f"route_question ————>")
     question = state["question"]
@@ -397,7 +268,34 @@ def decide_to_generate(state: GraphState):
         print(f"decide_to_generate ————> go to generate answer")
         return "generate"
 
+class GradeHallucinations(BaseModel):
+    """Binary score for hallucination present in generation answer."""
+    binary_score: str = Field(
+        description="Answer is grounded in the facts, 'yes' or 'no'"
+    )
+
+class GradeAnswer(BaseModel):
+    """Binary score to assess answer addressses question"""
+    binary_score: str = Field(
+        description="Answer addresses the question, 'yes' or 'no'"
+    )
+
 def grade_generation_v_documents_and_question(state: GraphState):
+    system = """
+    You are a grader assessing whether an LLM generation is grounded in / 
+    supported by a set of retrieved facts. \n
+
+    Give a binary score 'yes' or 'no', 
+    'Yes' means that the answer is grounded in / supported by the set of facts.
+    """
+    hallucination_prompt = ChatPromptTemplate.from_messages([
+        ("system", system),
+        ("human", "Set of facts: \n\n {documents} \n\n LLM generation: {generation}")
+    ])
+    llm = ModelTools.get_llm()
+    structured_llm_grader = llm.with_structured_output(GradeHallucinations)
+    hallucination_grader = hallucination_prompt | structured_llm_grader
+
     """
     Determines whether the generation is grounded in the document 
     and answers question.
@@ -405,7 +303,7 @@ def grade_generation_v_documents_and_question(state: GraphState):
     print(f"grade_generation_v_documents_and_question ————>")
     question = state["question"]
     documents = state["documents"]
-    generate = state["generation"]
+    generation = state["generation"]
 
     score = hallucination_grader.invoke({
         "documents": documents,
@@ -418,6 +316,20 @@ def grade_generation_v_documents_and_question(state: GraphState):
         print(f"grade_generation_v_documents_and_question ————> \
                 generation is grounded in documents")
         # 2.Check question-answering
+        # ---------------------------------------------------------------------------------
+        system = """
+        You are a grader assessing whether an answer addresses / resolves a question \n
+        Give a binary score 'yes' or 'no'.
+        'Yes' means that the answer resolves the question.
+        """
+        answer_prompt = ChatPromptTemplate.from_messages([
+            ("system", system),
+            ("human", "User question: \n\n {question} \n\n LLM generation: {generation}"),
+        ])
+        llm = ModelTools.get_llm()
+        structured_llm_grader = llm.with_structured_output(GradeAnswer)
+        answer_grader = answer_prompt | structured_llm_grader
+        # ---------------------------------------------------------------------------------
         score = answer_grader.invoke({
             "question": question,
             "generation": generation,
@@ -439,9 +351,6 @@ def grade_generation_v_documents_and_question(state: GraphState):
 #################
 # Compile Graph #
 #################
-from langgraph.graph import END, StateGraph, START
-from IPython.display import Image, display
-
 workflow = StateGraph(GraphState)
 
 # Define the nodes
@@ -483,9 +392,3 @@ workflow.add_conditional_edges(
 
 # Compile
 graph = workflow.compile()
-
-# Draw graph
-try:
-    display(Image(graph.get_graph().draw_mermaid_png()))
-except:
-    pass
